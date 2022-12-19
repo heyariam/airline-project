4a0a2b9f737e6c397f4f24aa2a060756eeaff8cd
Arianna Avalos <arianna.avalos15@gmail.com>
2022-12-19 17:32:15 -0500

    Home view updated

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app id="home">
       <NavBar />
-    <v-container fluid>
+    <v-container fluid class="main-cont">
       <div class="main-hero">
-        <v-row>
-          <v-col>
-            <div :style = "image">
-                <div style="position: relative" class="mt-16">
-                <h1 class="text-grey">Aquí comienza tu aventura,</h1>
-                <h3 class="text-pink">Busca tu vuelo</h3>
-                 </div>
-            </div>
+        <div class="container-wrap">
+          <v-row>   
+            <v-col cols="4">  
+              <div>
+                  <h1 class="text-grey">Aquí comienza tu aventura,</h1>
+                  <h3 class="text-pink">Busca tu vuelo</h3>
+              </div>
+            </v-col>
+            <v-col cols="8">
+              <v-img src="../assets/tripbackground.jpg"></v-img>
 
-          </v-col>
-        </v-row>
+            </v-col>
+          </v-row>
+        </div>
       </div>
     </v-container>
     <Footer />
@@ -24,14 +27,14 @@
 import { defineComponent } from "vue";
 import NavBar from "../components/NavBar.vue";
 import Footer from "../components/Footer.vue";
-import { url } from "inspector";
+
 // Components
 //import HelloWorld from '../components/HelloWorld.vue';
 export default defineComponent({
   name: "HomeView",
   setup() {
     return {
-      image: { backgroundImage: url("../assets/tripbackground.jpg")}
+
     };
   },
   components: {
@@ -40,3 +43,13 @@ export default defineComponent({
 },
 });
 </script>
+
+<style scoped>
+ *{
+  margin: 0px;
+  padding: 0px;
+ }
+  .main-hero{
+    background-color: "#FCF3CF";
+  }
+</style>
