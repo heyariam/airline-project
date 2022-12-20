97b0591b8a5b38dc63a937926c9b894124a53ef8
Arianna Avalos <arianna.avalos15@gmail.com>
2022-12-20 17:18:53 -0500

    dunno if this add person will work

---
+++ src/components/AddPassenger.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="person in people" :key="person.id">
+      {{ person.name }} ({{ person.age }}) - {{ person.email }}
+      <button @click="editPerson(person)">Edit</button>
+      <button @click="deletePerson(person)">Delete</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+
+
+  
+
+
--- src/components/FlightCards.vue
+++ src/components/FlightCards.vue
@@ -1,5 +1,6 @@
 <template>
   <v-card>
+    <!--need to add informtion related to flights and what i want to show here-->
     <v-card-title>{{ title }}</v-card-title>
     <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
     <v-card-text>{{ text }}</v-card-text>
