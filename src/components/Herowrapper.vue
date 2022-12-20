f7347de9dd012cf254518c825dc58eed596dae90
Arianna Avalos <arianna.avalos15@gmail.com>
2022-12-19 21:36:49 -0500

    Home view updated

--- src/components/Herowrapper.vue
+++ src/components/Herowrapper.vue
@@ -4,10 +4,42 @@
       <div class="container-wrap">
         <v-row>
           <v-col cols="12">
-
+            <v-card class="mx-auto rounded-xl" flat>
+              <v-img src="../assets/tripbackground.jpg" max-height="100%" max-width="100%" contain>
+                <v-row>
+                  <v-col class="glassmorph d-flex flex-column justify-center align-center" cols="12" lg="10" md="10">
+                    <v-card-title class="top ml-10">
+                       <h2 class="title1">Aquí inicia</h2>
+                    </v-card-title>
+                    <v-card-title class="ml-10 mt-n8">
+                      <h2 class="title2">tu nueva aventura</h2>
+                    </v-card-title>
+                    <v-card-text class="ml-10">
+                      <h3 class="white--text">#VuelaconTekton</h3>
+                    </v-card-text>
+                    <v-card-text class="ml-10">
+                      <v-row>
+                        <v-col cols="4">
+                          <v-card>
+                             <v-text-field label="Search your daily groceries" class="d-none d-sm-none d-md-flex" filled
+                                        prepend-inner-icon="mdi-magnify" solo flat
+                                        background-color="" rounded outlined success>
+                              </v-text-field>
+                            </v-card>
+                        </v-col>
+                        <v-col cols="2" class="marginLeft">
+                           <v-btn color="green" rounded dark x-large class="py-7 d-none d-sm-none d-md-flex">
+                             search
+                           </v-btn> 
+                        </v-col>
+                      </v-row>
+                    </v-card-text>
+                  </v-col>
+                </v-row>
+              </v-img>
+            </v-card>
           </v-col>
         </v-row>
-
       </div>
     </div>
   </v-container>
