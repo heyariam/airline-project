d2ac615bc6103e07a37a9dc156e25726ab5e2b45
Arianna Avalos <arianna.avalos15@gmail.com>
2022-12-20 16:24:47 -0500

    adding reusable components

--- src/components/Herowrapper.vue
+++ src/components/Herowrapper.vue
@@ -6,24 +6,7 @@
           <v-col cols="12">
             <v-card class="mx-auto rounded-xl" flat>
               <v-img src="../assets/tripbackground.jpg" max-height="100%" max-width="100%" contain>
-                <v-row align="center" justify="center" style="height:100%" dense>
-                  <v-col class="glassmorph d-flex flex-column justify-center align-center" cols="12" lg="10" md="10">
-                    <v-card-title class="top ml-10">
-                       <h2 class="title1">Aquí inicia tu nueva aventura</h2>
-                    </v-card-title>
-                    <v-card-title class="top ml-10">
-                       <h4 class="title1">#VuelaconTekton</h4>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-row>
-                        <v-col cols="10" class="card-glass">
-                          <SearchInput/>
-
-                        </v-col>
-                      </v-row>
-                    </v-card-text>
-                  </v-col>
-                </v-row>
+                <SearchInput/>
               </v-img>
             </v-card>
           </v-col>
@@ -49,14 +32,4 @@ export default{
   .main-hero{
     background-color: #f5c66f;
   }
-
-  .glassmorph{
-    background: rgba(255, 255, 255, 0.19);
-    border-radius: 16px;
-    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-    backdrop-filter: blur(5.2px);
-    -webkit-backdrop-filter: blur(5.2px);
-  }
-
- 
 </style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,11 +1,17 @@
 <template>
-  <v-form class="searchbox">
-    <v-text-field v-model="desde" label="Desde" />
-    <v-text-field v-model="hacia" label="Hacia" />
-    <v-text-field v-model="salida" label="Fecha salida" />
-    <v-text-field v-model="retorno" label="Fecha retorno" />
-    <v-btn @click="search">Buscar vuelos</v-btn>
-  </v-form>
+  <v-card class="glassmorph">
+    <v-card-title>Aquí comienza tu aventura</v-card-title><br/>
+    <h4 class="title1">#VuelaconTekton</h4>
+    <v-card-text>
+    <v-form class="searchbox">
+      <v-text-field v-model="desde" label="Desde" />
+      <v-text-field v-model="hacia" label="Hacia" />
+      <v-text-field v-model="salida" label="Fecha salida" />
+      <v-text-field v-model="retorno" label="Fecha retorno" />
+      <v-btn @click="search">Buscar vuelos</v-btn>
+    </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -43,13 +49,22 @@ export default {
 
 
 <style>
-.searchbox{
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-content:space-around;
-  gap: 2rem;
-  margin: 2rem;
-  padding: 1rem;
-}
+  .searchbox{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-content:space-around;
+    gap: 2rem;
+    margin: 2rem;
+    padding: 1rem;
+  }
+
+  .glassmorph{
+      background: rgba(255, 255, 255, 0.19);
+      border-radius: 16px;
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+      backdrop-filter: blur(5.2px);
+      -webkit-backdrop-filter: blur(5.2px);
+    }
+
 </style>
