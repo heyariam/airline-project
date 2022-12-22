8a6987a6ff586f2503cccbd2fcfd6ba0a9b5b258
Arianna Avalos <arianna.avalos15@gmail.com>
2022-12-22 13:00:23 -0500

    adding cards

--- src/components/FlightCards.vue
+++ src/components/FlightCards.vue
@@ -1,13 +1,37 @@
 <template>
-      <!--need to add informtion related to flights and what i want to show here-->
-  <div class="card">
-      <h2>{{ title }}</h2>
-
-    </div>
-
+ <v-container>
+      <v-row justify="space-around">
+        <v-card width="500" height="300">  
+          <v-card-text justify-content= "row">
+            <div class="font-weight-bold ml-8 mb-2">
+              <p>{hora salida}</p>
+                 <p>{ciudad}</p>
+            </div>
+             <div class="font-weight-bold ml-8 mb-2">
+                 <p>{duracion}</p>
+               <p>{img}</p>
+            </div>
+             <div class="font-weight-bold ml-8 mb-2">
+              <p>{hora llegada}</p>
+               <p>{ciudad}</p>
+            </div>
+             <div class="font-weight-bold ml-8 mb-2">
+               <div>
+                 <p>{precio}</p>
+               </div>
+               <div>
+                 <btn class="submit">comprar</btn>
+               </div>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-row>
+    </v-container>
 </template>
+
 
 <script>
 export default {
-}
+
+  }
 </script>
