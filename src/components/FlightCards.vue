95956d3d2b2a7665fb426564c9d9cbd2e400ddd9
Arianna Avalos <arianna.avalos15@gmail.com>
2022-12-22 12:20:47 -0500

    adding cards

--- src/components/FlightCards.vue
+++ src/components/FlightCards.vue
@@ -1,28 +1,13 @@
 <template>
-  <v-card>
-    <!--need to add informtion related to flights and what i want to show here-->
-    <v-card-title>{{ title }}</v-card-title>
-    <v-card-subtitle>{{ subtitle }}</v-card-subtitle>
-    <v-card-text>{{ text }}</v-card-text>
-  </v-card>
+      <!--need to add informtion related to flights and what i want to show here-->
+  <div class="card">
+      <h2>{{ title }}</h2>
+
+    </div>
+
 </template>
 
 <script>
 export default {
-  name: 'FlightCard',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    subtitle: {
-      type: String,
-      required: true
-    },
-    text: {
-      type: String,
-      required: true
-    }
-  }
 }
 </script>
