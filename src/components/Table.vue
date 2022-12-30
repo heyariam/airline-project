ca731c1fd4d00992d59e4e149ee19a0a3eac2137
Arianna Avalos <arianna.avalos15@gmail.com>
2022-12-30 15:19:44 -0500

    data

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,28 +1,29 @@
 <template>
-  <div>
-<tr v-for="(personal_data, k) in personal_data" :key="k">
-    <td scope="row" class="trashIconContainer">
-        <i class="far fa-trash-alt" @click="deleteRow(k, invoice_product)"></i>
-    </td>
-    <td>
-        <input class="form-control" type="text" v-model="invoice_product.product_no" />
-    </td>
-    <td>
-        <input class="form-control" type="text" v-model="invoice_product.product_name" />
-    </td>
-    <td>
-        <input class="form-control text-right" type="number" min="0" step=".01" v-model="invoice_product.product_price" @change="calculateLineTotal(invoice_product)"
-        />
-    </td>
-    <td>
-        <input class="form-control text-right" type="number" min="0" step=".01" v-model="invoice_product.product_qty" @change="calculateLineTotal(invoice_product)"
-        />
-    </td>
-    <td>
-        <input readonly class="form-control text-right" type="number" min="0" step=".01" v-model="invoice_product.line_total" />
-    </td>
-</tr>
-</div>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Option</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>
+          <input type="text" v-model="user.name" />
+        </td>
+        <td>
+          <input type="email" v-model="user.email" />
+        </td>
+        <td>
+          <select v-model="user.selectedOption">
+            <option v-for="option in options" v-bind:key="option">{{ option }}</option>
+          </select>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
 </template>
 
 <script>
