5eee46e43c7decf8a7676229cf3f33c245767e33
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-05 00:23:42 -0500

    adding validation

---
+++ src/components/formulario.js
@@ -0,0 +1,7 @@
+const expresiones = {
+  name: /^([ a-zA-Z0-9\sÁÉÍÓÚáéíóúÑñ]+)$/,
+  lastname: /^([ a-zA-Z0-9\sÁÉÍÓÚáéíóúÑñ]+)$/,
+  nationality: /^([ a-zA-Z0-9\sÁÉÍÓÚáéíóúÑñ]+)$/,
+  docnumber: /[A-Za-z0-9]+/i
+}
+
