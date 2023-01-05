6b8c5216cad789b664531bb8d9af6a1f462d5d14
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-04 23:10:53 -0500

    vuelidate added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,8 +10,7 @@
 import { defineComponent } from "vue";
 import NavBar from "../components/NavBar.vue";
 import Footer from "../components/Footer.vue";
-import Herowrapper from "./Herowrapper.vue";
-//import Register from "@/views/Register.vue";
+//import Herowrapper from "./Herowrapper.vue";
 import Table from "../components/Table.vue";
 
 
