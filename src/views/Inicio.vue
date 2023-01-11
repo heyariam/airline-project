be67cfae658f12070411a00e1995ab1ab1206358
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-11 10:04:33 -0500

    styles

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -26,3 +26,27 @@
     </v-carousel-item>
   </v-carousel>
 </template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        colors: [
+          'indigo',
+          'warning',
+          'pink darken-2',
+          'red lighten-1',
+          'deep-purple accent-4',
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+      }
+    },
+  }
+</script>
