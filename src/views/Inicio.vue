6011ce72a399082149050e61c62494a2260340e0
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-11 10:04:10 -0500

    styles

---
+++ src/views/Inicio.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-carousel
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        :color="colors[i]"
+        height="100%"
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="text-h2">
+            {{ slide }} Slide
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
