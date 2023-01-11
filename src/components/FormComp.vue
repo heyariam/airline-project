87d286a2db192c3087c1d3bf42b1083c23f056f8
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-11 16:37:08 -0500

    styles

--- src/components/FormComp.vue
+++ src/components/FormComp.vue
@@ -1,169 +1,180 @@
 <template>
-           <div class="info_container spacing-playground pa-6">
+    <div class="info_container spacing-playground pa-6">
 
-            <v-card class="inputs_card">
-                <!--Título-->
-                <h4 class="font-weight-bold text-center pa-2">Ingresar datos</h4>
-                    <!--Grupo Formulario-->
-                <v-form ref="form"> 
-                    <!--Grupo: Nombre-->
-                    <div>
-                        <v-text-field v-model="name" label="Nombre" 
-                        :rules="validateName" ></v-text-field>
-                 </div><br/>
-                 <!--Grupo: Apellido-->
-                 <div>
-                            <v-text-field v-model="lastname" label="Apellido"
-                            :rules="validateLastName" > </v-text-field>
-                </div><br/>
-                 <!--Grupo: Nacionalidad-->
-                 <div>
-                        <v-text-field v-model="nationality" label="Nacionalidad"
-                        :rules="validateNationality"> </v-text-field>
-                </div>   <br/>
-                 <!--Grupo: Tipo de documento-->
-                <div> 
+        <v-card class="inputs_card">
+            <!--Título-->
+            <h4 class="font-weight-bold text-center pa-2">Ingresar datos</h4>
+            <!--Grupo Formulario-->
+            <v-form ref="form">
+                <!--Grupo: Nombre-->
+                <div>
+                    <v-text-field v-model="name" label="Nombre" :rules="validateName"></v-text-field>
+                </div><br />
+                <!--Grupo: Apellido-->
+                <div>
+                    <v-text-field v-model="lastname" label="Apellido" :rules="validateLastName"> </v-text-field>
+                </div><br />
+                <!--Grupo: Nacionalidad-->
+                <div>
+                    <v-text-field v-model="nationality" label="Nacionalidad" :rules="validateNationality">
+                    </v-text-field>
+                </div> <br />
+                <!--Grupo: Tipo de documento-->
+                <div>
                     <!--<v-select :items="items" v-model="doctype" label="Tipo de documento"
                     >{{items}}</v-select>-->
-                    <v-select
-                        v-model="doctype"
-                        :items="items"
-                        item-text="document"
-                        label="Tipo de documento"
-                        return-object
-                        single-line
-                        :rules="validateDocument"
-                        > {{items}}
+                    <v-select v-model="doctype" :items="items" item-text="document" label="Tipo de documento"
+                        return-object single-line :rules="validateDocument"> {{ items }}
                     </v-select>
-                   
-                  
+
+
                 </div>
-           <br/>
-                 <!--Grupo: Numero documento-->
-                 <div>
+                <br />
+                <!--Grupo: Numero documento-->
+                <div>
 
-                        <v-text-field v-model="docnumber" label="Número de documento" :rules="validateDocNumber" ></v-text-field>
+                    <v-text-field v-model="docnumber" label="Número de documento"
+                        :rules="validateDocNumber"></v-text-field>
 
-                </div><br/>
-                 <!--Grupo: Button-->
+                </div><br />
+                <!--Grupo: Button-->
                 <div>
                     <v-card-actions>
-                        <v-btn class="button2" @click="navigateToPage">
+                        <v-btn color="error" @click="navigateToPage">
                             Cancelar
                         </v-btn>
                         <v-spacer></v-spacer>
-        
-                                <v-btn 
-                                elevation="2"
-                                @click="reset">
-                                    Limpiar
-                                </v-btn>
-    
-                        <v-btn 
-                        elevation="2"
-                        color="#FA8072"
-                        @click="saveData(haveID)"> Agregar </v-btn>
-                
+                        <v-btn class="ma-2" outlined color="indigo" @click="reset">
+                            Limpiar
+                        </v-btn>
+
+                        <v-btn elevation="2" color="success" @click="saveData(haveID)"> Agregar </v-btn>
+
                     </v-card-actions>
                 </div>
             </v-form>
-       
-        <div class="usersdata_card">
-            <div class="overflow-x-auto relative sm:rounded-lg"> 
-            <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
-                <thead> 
-                    <h4>Datos pasajeros</h4>
-                </thead>
-                    <tbody>
-                        <tr class="bg white border-b dark:bg-gray-800 dark:border-gray-700" v-for="data in passengerData" :key="data.id">
-                            <th class="py-4 px-6">{{data.name}}</th>
-                            <th class="py-4 px-6">{{data.lastname}}</th>
-                            <th class="py-4 px-6">{{data.nationality}}</th>
-                            <th class="py-4 px-6">{{data.doctype}}</th>
-                            <th class="py-4 px-6">{{data.docnumber}}</th>
-                       
-                        <td>
-                           <div> 
-                                <v-btn class="button_styled" @click="getData(data.id)"> Editar </v-btn>
-                            </div>
-                            <div> 
-                                <v-btn class="button_styled" @click="deleteData(data.id)"> Eliminar </v-btn>
-                            </div>
-                
-                        </td>
-                    </tr>
-                    </tbody>
-            </table>
-         
-        </div>
 
-        <div> 
-            
-            <v-btn 
-            elevation="2"
-            >Guardar información</v-btn>
-        </div>
+            <div class="usersdata_card">
+                <div class="overflow-x-auto relative sm:rounded-lg">
+                    <v-card-actions>
+                        <v-btn color="orange lighten-2" text>
+                            Explore
+                        </v-btn>
+
+                        <v-spacer></v-spacer>
+
+                        <v-btn icon @click="show = !show">
+                            <v-icon>{{ show? 'mdi-chevron-up': 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                    </v-card-actions>
+
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <v-divider></v-divider>
+
+                            <v-card>
+                                <table class="w-full text-sm text-center text-gray-500 dark:text-gray-400">
+                                    <thead>
+                                        <h4>Datos pasajeros</h4>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="bg white border-b dark:bg-gray-800 dark:border-gray-700"
+                                            v-for="data in passengerData" :key="data.id">
+                                            <th class="py-4 px-6">{{ data.name }}</th>
+                                            <th class="py-4 px-6">{{ data.lastname }}</th>
+                                            <th class="py-4 px-6">{{ data.nationality }}</th>
+                                            <th class="py-4 px-6">{{ data.doctype }}</th>
+                                            <th class="py-4 px-6">{{ data.docnumber }}</th>
+
+                                            <td>
+                                                <div>
+                                                    <v-btn class="button_styled" @click="getData(data.id)"> Editar
+                                                    </v-btn>
+                                                </div>
+                                                <div>
+                                                    <v-btn class="button_styled" @click="deleteData(data.id)"> Eliminar
+                                                    </v-btn>
+                                                </div>
+
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table><br>
+                                <div>
+
+                                    <v-btn color="success" elevation="2">Guardar información</v-btn>
+                                </div>
+
+                            </v-card>
+                        </div>
+                    </v-expand-transition>
+
+
+                </div>
+
+
+            </div>
+        </v-card>
     </div>
-    </v-card>
-</div>
 
 </template>
 
 <script>
 
-export default{
+export default {
     name: 'FormComp',
-     components: {
-},
+    components: {
+    },
     data() {
-        return{
-                items: [
-                { document: 'DNI', regex: /^([0-9]+)$/ , maxLength: 8 },
-                { document: 'Pasaporte', regex: /^([0-9]+)$/, maxLength: 9  },
+        return {
+            show: false,
+            items: [
+                { document: 'DNI', regex: /^([0-9]+)$/, maxLength: 8 },
+                { document: 'Pasaporte', regex: /^([0-9]+)$/, maxLength: 9 },
                 { document: 'C.E', regex: /^([a-zA-Z]+)$/, maxLength: 9 }
-                ],
-        valid: true,
-        id: "",
-        name: "",
-        validateName: [
-        v => !!v || 'Obligatorio',
-        v => /^([ a-zA-Z\sÁÉÍÓÚáéíóúÑñ]+)$/.test(v) || 'Ingresa un dato válido (solo letras y espacios permitidos)',
-      ],
-        lastname: "",
-        validateLastName: [
-        v => !!v || 'Obligatorio',
-        v => /^([ a-zA-Z\sÁÉÍÓÚáéíóúÑñ]+)$/.test(v) || 'Ingresa un dato válido (solo letras y espacios permitidos)',
-      ],
-        nationality: "",
-        validateNationality: [
-        v => !!v || 'Obligatorio',
-        v => /^([a-zA-Z]+)$/.test(v) || 'Ingresa un dato válido (solo letras permitidas)',
-      ],
-        doctype: "",
-        validateDocument: [
-        v => (v =='DNI' && v.length <= item.maxLength) || 'Máximo 8 carácteres',
-        v => (v =='Pasaporte' && v.length > 9) || 'Máximo 9 carácteres',
-        v => (v =='C.E' && v.length > 9) || 'Máximo 9 carácteres',
+            ],
+            valid: true,
+            id: "",
+            name: "",
+            validateName: [
+                v => !!v || 'Obligatorio',
+                v => /^([ a-zA-Z\sÁÉÍÓÚáéíóúÑñ]+)$/.test(v) || 'Ingresa un dato válido (solo letras y espacios permitidos)',
+            ],
+            lastname: "",
+            validateLastName: [
+                v => !!v || 'Obligatorio',
+                v => /^([ a-zA-Z\sÁÉÍÓÚáéíóúÑñ]+)$/.test(v) || 'Ingresa un dato válido (solo letras y espacios permitidos)',
+            ],
+            nationality: "",
+            validateNationality: [
+                v => !!v || 'Obligatorio',
+                v => /^([a-zA-Z]+)$/.test(v) || 'Ingresa un dato válido (solo letras permitidas)',
+            ],
+            doctype: "",
+            validateDocument: [
+                v => (v == 'DNI' && v.length <= item.maxLength) || 'Máximo 8 carácteres',
+                v => (v == 'Pasaporte' && v.length > 9) || 'Máximo 9 carácteres',
+                v => (v == 'C.E' && v.length > 9) || 'Máximo 9 carácteres',
 
-      ],
-        docnumber: "",
-        validateDocNumber: [
-        v => !!v || 'Obligatorio',
-        v => (v =='DNI'&& this.items.regex.test(v)) || 'Solo numeros permitidos',
-        v => (v =='Pasaporte' && this.items.regex.test(v)) || 'Solo numeros permitidos',
-        v => (v =='C.E' && this.items.regex.test(v)) || 'Solo números y letras permitidas',
-      ],
-        haveID:null,
-        passengerData:[
-          
-  ]
+            ],
+            docnumber: "",
+            validateDocNumber: [
+                v => !!v || 'Obligatorio',
+                v => (v == 'DNI' && this.items.regex.test(v)) || 'Solo numeros permitidos',
+                v => (v == 'Pasaporte' && this.items.regex.test(v)) || 'Solo numeros permitidos',
+                v => (v == 'C.E' && this.items.regex.test(v)) || 'Solo números y letras permitidas',
+            ],
+            haveID: null,
+            passengerData: [
 
-     }
-},
+            ]
+
+        }
+    },
     methods: {
-        getData(id){
-            this.haveID = true 
-            var dataPass = this.passengerData.filter((data =>(data.id == id)))
+        getData(id) {
+            this.haveID = true
+            var dataPass = this.passengerData.filter((data => (data.id == id)))
             //assign to input
             this.name = dataPass[0].name
             this.lastname = dataPass[0].lastname
@@ -172,70 +183,71 @@ export default{
             this.docnumber = dataPass[0].docnumber
 
         },
-        saveData(haveID){
+        saveData(haveID) {
             //update
-            if(haveID){
-                var dataPass = this.passengerData.filter((data =>(data.id == haveID)))
+            if (haveID) {
+                var dataPass = this.passengerData.filter((data => (data.id == haveID)))
                 dataPass[0].name = this.name
                 dataPass[0].lastname = this.lastname
                 dataPass[0].nationality = this.nationality
                 dataPass[0].doctype = this.doctype
                 dataPass[0].docnumber = this.docnumber
-            }else {
+            } else {
                 // save
-                const id= this.passengerData.length + 1
+                const id = this.passengerData.length + 1
                 const data = {
-                id: id,
-                name: this.name,
-                lastname: this.lastname,
-                nationality: this.nationality,
-                doctype: this.doctype,
-                docnumber: this.docnumber,
-            }
+                    id: id,
+                    name: this.name,
+                    lastname: this.lastname,
+                    nationality: this.nationality,
+                    doctype: this.doctype,
+                    docnumber: this.docnumber,
+                }
                 this.passengerData.push(data);
             }
-            
+
         },
-        deleteData(id){
-         var index = this.passengerData.findIndex((data) => data.id == id)
-         this.passengerData.splice(index, 1)
+        deleteData(id) {
+            var index = this.passengerData.findIndex((data) => data.id == id)
+            this.passengerData.splice(index, 1)
         },
-        reset () {
-        this.$refs.form.reset()
+        reset() {
+            this.$refs.form.reset()
         },
-            navigateToPage() {
-            this.$router.push({ path: '/inicio'})
-            },
-        }        
-        }
+        navigateToPage() {
+            this.$router.push({ path: '/inicio' })
+        },
+    }
+}
 
 
 </script>
 
 <style scoped>
- *{
+* {
     margin: 0px;
     padding: 0px;
- }
- .info_container{
+}
+
+.info_container {
     width: 100%;
     background-color: var(--travel-color-blue-500);
     display: flex;
     flex-direction: column;
     align-items: center;
- }
+}
 
- .inputs_card{
+.inputs_card {
     background-color: var(--travel-color-white);
     width: 60%;
     border-radius: 2rem;
     margin: 0.5rem;
     padding: 2rem;
     border-radius: 30px;
- }
-h4{
-   
-    font-size:1.50rem;
 }
 
+h4 {
+
+    font-size: 1.50rem;
+}
 </style>
