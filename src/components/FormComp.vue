43ac1d9a7d3723cffb0619c8e085f0725e291789
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-11 14:18:16 -0500

    styles

--- src/components/FormComp.vue
+++ src/components/FormComp.vue
@@ -53,13 +53,13 @@
                         </v-btn>
                         <v-spacer></v-spacer>
         
-                                <v-btn @click="reset">
+                                <v-btn class="button2"
+                                @click="reset">
                                     Limpiar
                                 </v-btn>
     
                         <v-btn 
-                        color="primary"
-                        text
+                        class="button_styled"
                         @click="saveData(haveID)"> Agregar </v-btn>
                 
                     </v-card-actions>
@@ -82,10 +82,10 @@
                        
                         <td>
                            <div> 
-                                <v-btn @click="getData(data.id)"> Editar </v-btn>
+                                <v-btn class="button_styled" @click="getData(data.id)"> Editar </v-btn>
                             </div>
                             <div> 
-                                <v-btn @click="deleteData(data.id)"> Eliminar </v-btn>
+                                <v-btn class="button_styled" @click="deleteData(data.id)"> Eliminar </v-btn>
                             </div>
                 
                         </td>
@@ -97,7 +97,7 @@
 
         <div> 
             
-            <v-btn class="savebutton">Guardar información</v-btn>
+            <v-btn class="button_styled">Guardar información</v-btn>
         </div>
     </div>
     </v-card>
@@ -237,8 +237,8 @@ h4{
     font-size:1.50rem;
 }
 
-.savebutton{
-display: flex;
+.button_styled{
+ display: flex;
  align-items: center;
  justify-content: center;
  gap: 10px;
@@ -259,13 +259,83 @@ display: flex;
  box-shadow: -4px -2px 16px 0px #ffffff, 4px 2px 16px 0px rgb(95 157 231 / 48%);
 }
 
-.savebutton:hover {
+.button_styled:hover {
  color: #516d91;
  background-color: #E5EDF5;
  box-shadow: -2px -1px 8px 0px #ffffff, 2px 1px 8px 0px rgb(95 157 231 / 48%);
 }
 
-.savebutton:active {
+.button_styled:active {
  box-shadow: none;
 }
+
+.button2 {
+  display: inline-block;
+  transition: all 0.2s ease-in;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  color: #090909;
+  padding: 0.7em 1.7em;
+  font-size: 18px;
+  border-radius: 0.5em;
+  background: #e8e8e8;
+  border: 1px solid #e8e8e8;
+  box-shadow: 6px 6px 12px #c5c5c5,
+             -6px -6px 12px #ffffff;
+}
+
+.button2:active {
+  color: #666;
+  box-shadow: inset 4px 4px 12px #c5c5c5,
+             inset -4px -4px 12px #ffffff;
+}
+
+.button2:before {
+  content: "";
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%) scaleY(1) scaleX(1.25);
+  top: 100%;
+  width: 140%;
+  height: 180%;
+  background-color: rgba(0, 0, 0, 0.05);
+  border-radius: 50%;
+  display: block;
+  transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+  z-index: -1;
+}
+
+.button2:after {
+  content: "";
+  position: absolute;
+  left: 55%;
+  transform: translateX(-50%) scaleY(1) scaleX(1.45);
+  top: 180%;
+  width: 160%;
+  height: 190%;
+  background-color: #009087;
+  border-radius: 50%;
+  display: block;
+  transition: all 0.5s 0.1s cubic-bezier(0.55, 0, 0.1, 1);
+  z-index: -1;
+}
+
+.button2:hover {
+  color: #ffffff;
+  border: 1px solid #009087;
+}
+
+.button2:hover:before {
+  top: -35%;
+  background-color: #009087;
+  transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+}
+
+.button2:hover:after {
+  top: -45%;
+  background-color: #009087;
+  transform: translateX(-50%) scaleY(1.3) scaleX(0.8);
+}
+
 </style>
