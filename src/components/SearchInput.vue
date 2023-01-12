86ef54abbc9aa0d3a22dd1659b202b43654e8a4a
Arianna Avalos <arianna.avalos15@gmail.com>
2023-01-12 16:50:38 -0500

    working on responsive

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,39 +1,43 @@
 <template>
   <v-main>
     <v-row align="center" justify="center" style="height:100%" dense>
-      <v-col class="glassmorph d-flex flex-column justify-center align-center" cols="12" lg="10" md="10" sm="6" xs="2">
+      <v-col class="glassmorph" cols="12" lg="10" md="10" sm="6">
         <v-card-title class="card-title">Aquí comienza tu aventura</v-card-title>
-        <div>
-      
-        <v-form class="searchbox">
-          <v-select :items="items" label="Desde">{{ items }}</v-select>
-          <v-select :items="items" label="Hacia"></v-select>
-
-          <v-menu v-model="fromDateMenu" :close-on-content-click="false" :nudge-right="40" lazy
-            transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
-            <template v-slot:activator="{ on }">
-              <v-text-field label="Fecha salida" readonly :value="fromDateDisp" v-on="on"></v-text-field>
-            </template>
-            <v-date-picker locale="en-in" :min="minDate" :max="maxDate" v-model="fromDateVal" no-title
-              @input="fromDateMenu = false"></v-date-picker>
-          </v-menu>
-          <v-menu v-model="toDateMenu" :close-on-content-click="false" :nudge-right="40" lazy
-            transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
-            <template v-slot:activator="{ on }">
-              <v-text-field label="Fecha retorno" readonly :value="toDateDisp" v-on="on"></v-text-field>
-            </template>
-            <v-date-picker locale="en-in" :min="minDate" :max="maxDate" v-model="toDateVal" no-title
-              @input="toDateMenu = false"></v-date-picker>
-          </v-menu>
-        <div>
-          <v-btn @click="navigateToPage" rounded>
-            Registrar</v-btn></div>
-        </v-form>
-   
-    </div>
-    </v-col>
-  </v-row>
-</v-main>
+        <div class="searchbox">
+            <div>
+              <v-select :items="items" label="Desde">{{ items }}</v-select>
+            </div>
+            <div> 
+              <v-select :items="items" label="Hacia"></v-select>
+            </div>
+          <div>
+            <v-menu v-model="fromDateMenu" :close-on-content-click="false" :nudge-right="40" lazy
+              transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
+              <template v-slot:activator="{ on }">
+                <v-text-field label="Fecha salida" readonly :value="fromDateDisp" v-on="on"></v-text-field>
+              </template>
+              <v-date-picker locale="en-in" :min="minDate" :max="maxDate" v-model="fromDateVal" no-title
+                @input="fromDateMenu = false"></v-date-picker>
+            </v-menu>
+          </div>
+          <div>
+            <v-menu v-model="toDateMenu" :close-on-content-click="false" :nudge-right="40" lazy
+              transition="scale-transition" offset-y full-width max-width="290px" min-width="290px">
+              <template v-slot:activator="{ on }">
+                <v-text-field label="Fecha retorno" readonly :value="toDateDisp" v-on="on"></v-text-field>
+              </template>
+              <v-date-picker locale="en-in" :min="minDate" :max="maxDate" v-model="toDateVal" no-title
+                @input="toDateMenu = false"></v-date-picker>
+            </v-menu>
+          </div>
+          <div>
+            <v-btn @click="navigateToPage" rounded>
+              Registrar</v-btn>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-main>
 </template>
 
 <script>
